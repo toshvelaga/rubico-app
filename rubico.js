500dba314edc4435b5e0dc581d072116f81b25e7
Tosh Velaga <toshvelaga@gmail.com>
2021-06-26 01:15:36 -0500

    async IIFE

--- rubico.js
+++ rubico.js
@@ -1,7 +1,25 @@
 const rubico = require('rubico')
-const pipe = require('rubico/pipe')
-const tap = require('rubico/tap')
-const defaultsDeep = require('rubico/x/defaultsDeep')
+const fetch = require('node-fetch');
+// const pipe = require('rubico/pipe')
+// const tap = require('rubico/tap')
+// const defaultsDeep = require('rubico/x/defaultsDeep')
 
-console.log('hello')
-console.log(rubico)
+const {
+  pipe, tap,
+  switchCase, tryCatch,
+  fork, assign, get, pick, omit,
+  map, filter, reduce, transform, flatMap,
+  and, or, not, any, all,
+  eq, gt, lt, gte, lte,
+  thunkify, always,
+  curry, __,
+} = rubico
+
+;(async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const responseData = await response.json()
+    console.log(responseData)
+})()
+
+// console.log('hello')
+// console.log(rubico)
