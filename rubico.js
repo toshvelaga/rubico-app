bf688377fa446a0baa87ee8e6416c5cbd8ea0d02
Tosh Velaga <toshvelaga@gmail.com>
2021-06-26 00:31:56 -0500

    add

---
+++ rubico.js
@@ -0,0 +1,7 @@
+const rubico = require('rubico')
+const pipe = require('rubico/pipe')
+const tap = require('rubico/tap')
+const defaultsDeep = require('rubico/x/defaultsDeep')
+
+console.log('hello')
+console.log(rubico)
